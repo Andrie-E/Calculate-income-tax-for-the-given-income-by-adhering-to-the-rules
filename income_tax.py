03fdd24090933660f582535d6181365057c6d300
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 20:47:00 +0800

    first adhere

--- income_tax.py
+++ income_tax.py
@@ -2,9 +2,14 @@
 
 # Pseudocode
 
-# identify the variables
 # if it is adhere to $10000
+def income_tax(income):
+    if income <= 10000:
+        tax = 0
+        print(tax)
 # if it is adhere to $20000
 # else and return
 # user input code
+user_income = float(input("provide your income: "))
+income_tax = income_tax(user_income)
 # print the results
