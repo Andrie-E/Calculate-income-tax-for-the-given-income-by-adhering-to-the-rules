7b899840cf560ba6095733b7aa8880e1facdae07
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 20:42:38 +0800

    pseudocode

--- income_tax.py
+++ income_tax.py
@@ -1,1 +1,10 @@
 # Calculate income tax for the given income by adhering to the rules
+
+# Pseudocode
+
+# identify the variables
+# if it is adhere to $10000
+# if it is adhere to $20000
+# else and return
+# user input code
+# print the results
